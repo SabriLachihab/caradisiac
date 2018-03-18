ab8744cbf5abf515558e391a1e714cab5051dac3
Sabri Lachihab <sabrilachihab@gmail.com>
2018-03-19 00:47:10 +0100

    Create JSON from Cardisiac Car
    * install the scrapping of cardiasiac
    * index.js -> server for search the informations 
    * Create json with models of car with the brand

---
+++ index.js
@@ -0,0 +1,31 @@
+const { getBrands } = require('node-car-api');
+const { getModels } = require('node-car-api');
+var fs = require('fs');
+
+async function print () {
+  const models = await getModels('AUDI');
+  console.log(models);
+}
+
+async function createjson() {
+  const brands = await getBrands();
+  console.log(brands);
+  const cars = [];
+  brands.forEach(async brand => {
+    var carsWithBrand = await getModels(brand);
+    carsWithBrand.forEach(car => {
+      cars.push(car);
+    });
+  fs.writeFile('cars.json', JSON.stringify(cars, null, 4), 'utf8', function(error){
+        if(error)
+        {
+          return console.log(error);
+        }
+          else{
+            console.log("we found "+ cars.length +" cars in Cardisiac");
+          }
+          })
+	})
+}
+
+createjson()
